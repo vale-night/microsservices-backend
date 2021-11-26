d7fb747d9002417c496c2d804c6d7f6a01395694
Arthur Tavares <arthurabreu00@gmail.com>
2021-11-26 20:19:14 -0300

    improvements databse config

--- events/config/database.php
+++ events/config/database.php
@@ -1,8 +1,22 @@
 <?php
 return [
-    'default' => 'mongodb',
+    'default' => env('DB_CONNECTION', 'mongodb'),
     'connections' => [
         'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'events'),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'options' => [
+                // here you can pass more settings to the Mongo Driver Manager
+                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
+
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
+            ],
+        ],
+        'mongodb_prod' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_DSN'),
             'database' => env('DB_DATABASE', 'events'),
