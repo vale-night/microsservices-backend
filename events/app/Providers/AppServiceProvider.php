2b1e11a07a853b8dabc357f5dba0d37b9cedb7a8
Arthur Tavares <arthurabreu00@gmail.com>
2021-11-25 23:30:41 -0300

    create mutiple connections

--- events/app/Providers/AppServiceProvider.php
+++ events/app/Providers/AppServiceProvider.php
@@ -14,7 +14,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->register(\Jenssegers\Mongodb\MongodbServiceProvider::class);
-        $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         $this->app->register(\App\Providers\RepositoryServiceProvider::class);
         $this->app->register(\Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
         $this->app->register(\Anik\Form\FormRequestServiceProvider::class);
--- events/config/database.php
+++ events/config/database.php
@@ -4,11 +4,8 @@ return [
     'connections' => [
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
+            'dsn' => env('DB_DSN'),
             'database' => env('DB_DATABASE', 'events'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
             'options' => [
                 // here you can pass more settings to the Mongo Driver Manager
                 // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
